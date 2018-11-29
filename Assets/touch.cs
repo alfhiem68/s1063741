443e72433a504fa6a70935f686e2a2e391980ac7
alfhiem68 <alfhiem68@gmail.com>
2018-11-30 03:39:49 +0800

    q8

---
+++ Assets/touch.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class touch : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public GameObject person;
+
+    void OnCollisionEnter(Collision collision)
+    {
+
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
